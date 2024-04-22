405fa1f22ea2f99df86bcf2fff4cec5b390663d6
pepecalero <josep.calero@urv.cat>
2024-04-22 02:15:25 +0200

    Setup.py torch dependencies changes

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import setup, find_packages
+import sys
+
+# Get the python version
+python_version = sys.version_info[:2]
+
+wheel_urls = {
+    (3, 11): {
+        'torch': 'https://download.pytorch.org/whl/cpu/torch-2.0.1%2Bcpu-cp311-cp311-linux_x86_64.whl',
+        'torchvision': 'https://download.pytorch.org/whl/cpu/torchvision-0.15.2%2Bcpu-cp311-cp311-linux_x86_64.whl',
+    },
+    (3, 10): {
+        'torch': 'https://download.pytorch.org/whl/cpu/torch-2.0.1%2Bcpu-cp310-cp310-linux_x86_64.whl',
+        'torchvision': 'https://download.pytorch.org/whl/cpu/torchvision-0.15.2%2Bcpu-cp310-cp310-linux_x86_64.whl',
+    },
+    # Add more versions if needed
+}
+
+
 
 setup(
     name='off_sample_orchestrator',
@@ -16,11 +34,9 @@ setup(
         'protobuf==4.21.12',
         'lithops_serve @ git+https://github.com/ZikBurns/lithops-serve'
     ],
-extras_require={
-        ':python_version=="3.11"': [
-            'https://download.pytorch.org/whl/cpu/torch-2.0.1%2Bcpu-cp311-cp311-linux_x86_64.whl',
-            'https://download.pytorch.org/whl/cpu/torchvision-0.15.2%2Bcpu-cp311-cp311-linux_x86_64.whl'
-        ],
-    },
+    dependency_links = [
+        wheel_urls[python_version]['torch'],
+        wheel_urls[python_version]['torchvision'],
+    ],
 )
 
